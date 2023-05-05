3813abd8dfb2df4657940d8a9d84e7f79190e04d
zephyras <hkw0101@gmail.com>
2023-05-06 00:20:54 +0900

    RestApi Movies Controller
    RestApi Movies Controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
 
+//@Module을 데코레이터라고 칭한다. (클래스에 함수 기능을 추가해줄수걸 말한다.)
+//예시로 아이스크림 위에 초콜릿 칩을 뿌리는것 같다.
+//controller는 기본적으로 url을 가져오고 함수를 실행하는 역할을 한다. node.js 같은 라우터 역활이다.
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [MoviesController],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+declare const module: any;
+//NestFactory.create(AppModule)을 호출후 3000포트를 리스닝 해준다.
+//controller, sevice가 모듈화 되어 AppModule에 담겨져 있다.
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
